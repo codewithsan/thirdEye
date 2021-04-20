7f6e0835ce05f8340334a29b77341131c7464614
Sandeep Kumar <67945191+san07364@users.noreply.github.com>
2021-04-20 21:25:26 +0530

    Delete config.dart

--- lib/util/config.dart
+++
@@ -1,4 +0,0 @@
-const String API_KEY = "cae20b0c96244b28b30775d9a9983cf4";
-const String URL = "codewithsan.cognitiveservices.azure.com";
-const String DESCRIBE_ENDPOINT = "/vision/v3.0/describe";
-const String OCR_ENDPOINT = "/vision/v3.0/ocr";
