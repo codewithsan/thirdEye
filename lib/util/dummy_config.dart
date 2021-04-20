3f7c3e3d3293f079fe5028a191340404ce68afd5
san07364 <san07364@gmail.com>
2021-04-20 20:18:54 +0530

    first commit

---
+++ lib/util/dummy_config.dart
@@ -0,0 +1,6 @@
+//make a file config.dart and paste below code with your api key.
+
+const String API_KEY = "your-api-key";
+const String URL = "codewithsan.cognitiveservices.azure.com";
+const String DESCRIBE_ENDPOINT = "/vision/v3.0/describe";
+const String OCR_ENDPOINT = "/vision/v3.0/ocr";
